6d89eb0becab6381b17a11a5f47f02dd575d583d
erenustun <eren.uestuen@gmail.com>
2023-06-07 07:31:21 +0200

    refactor: app.module.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from '@module/app.module'
+import { AppModule } from '@/src/app.module'
 import {
   FastifyAdapter,
   NestFastifyApplication,
