388a577ff48877b0b51fed8248b4564064d5aff6
erenustun <eren.uestuen@gmail.com>
2023-06-07 07:30:34 +0200

    fix: database config to connect postgres connection

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -10,9 +10,9 @@ export class DatabaseConfig implements TypeOrmOptionsFactory {
       database: process.env.DB_NAME,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
-      port: parseInt(process.env.DB_PORT as string, 10),
-      autoLoadEntities: true,
-      entities: ['dist/**/*.entity.js'],
+      port: parseInt(process.env.DB_PORT as string, 10) || 54320,
+      entities: ['dist/**/*.model.js'],
+      synchronize: true,
     }
   }
 }
