4f92cefff5c171147ea04b5da17a33f39c72d594
erenustun <eren.uestuen@gmail.com>
2023-06-07 07:11:58 +0200

    feat: typeorm configuration

---
+++ src/config/database.config.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common'
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm'
+
+@Injectable()
+export class DatabaseConfig implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      port: parseInt(process.env.DB_PORT as string, 10),
+      autoLoadEntities: true,
+      entities: ['dist/**/*.entity.js'],
+    }
+  }
+}
