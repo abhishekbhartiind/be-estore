13754d2915068fb45f1b5705fc3170dfed5f8173
erenustun <eren.uestuen@gmail.com>
2023-06-07 07:32:09 +0200

    fix: missing feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { DatabaseConfig } from '@config/database.config'
 import { ConfigModule } from '@nestjs/config'
-import { ProductModule } from '@feature/product/product.module'
 
 @Module({
   imports: [
@@ -10,7 +9,6 @@ import { ProductModule } from '@feature/product/product.module'
     TypeOrmModule.forRootAsync({
       useClass: DatabaseConfig,
     }),
-    ProductModule,
   ],
 })
 export class AppModule {}
