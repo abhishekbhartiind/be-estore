d4b3afc69aaffacbc63927569e5b517ed2390447
erenustun <eren.uestuen@gmail.com>
2023-06-07 19:14:53 +0200

    feat: product resolver (mvp) with mandatory query to run a apollo server

--- src/features/product/product.module.ts
+++ src/features/product/product.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ProductService } from '@feature/product/product.service'
 import { Product } from '@feature/product/product.model'
+import { ProductResolver } from './product.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
-  providers: [ProductService],
+  providers: [ProductService, ProductResolver],
 })
 export class ProductModule {}
