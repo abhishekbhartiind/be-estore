5178a2a1711f8c225bd1481725a23851e0ea23e3
erenustun <eren.uestuen@gmail.com>
2023-06-07 19:20:15 +0200

    fix: prettier

--- src/features/product/product.module.ts
+++ src/features/product/product.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ProductService } from '@feature/product/product.service'
 import { Product } from '@feature/product/product.model'
-import { ProductResolver } from './product.resolver';
+import { ProductResolver } from './product.resolver'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
