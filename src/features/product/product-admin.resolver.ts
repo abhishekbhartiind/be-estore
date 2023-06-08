1777fd1758633025e22f4287e8db6c04606833e1
erenustun <eren.uestuen@gmail.com>
2023-06-08 15:38:55 +0200

    fix: unused imports

--- src/features/product/product-admin.resolver.ts
+++ src/features/product/product-admin.resolver.ts
@@ -1,25 +1,11 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
-import {
-  Product,
-  ProductsFetchResponse,
-} from '@feature/product/model/product.model'
-import { PaginationArgs } from '@feature/product/dto/pagination.args'
-import { SortArgs } from '@feature/product/dto/sort.args'
-import { FilterArgs } from '@feature/product/dto/filter.args'
+import { Args, Mutation, Resolver } from '@nestjs/graphql'
+import { Product } from '@feature/product/model/product.model'
 import { ProductService } from '@feature/product/product.service'
-import {
-  SORT_DIR,
-  SORT_OPTION,
-} from '@feature/product/constant/sort-options.constant'
-import { ProductRating } from '@feature/product/model/rating.model'
 import { RoleGuard } from '@shared/features/auth/guard/role.guard'
 import { JwtAuthGuard } from '@shared/features/auth/guard/jwt-auth.guard'
 import { Role } from '@feature/user/enum/role.enum'
 import { HasRoles } from '@shared/decorator/role.decorator'
 import { UseGuards } from '@nestjs/common'
-import { CreateRatingInput } from '@feature/product/dto/create-rating.input'
-import { CurrentUser } from '@shared/decorator/current-user.decorator'
-import { User } from '@feature/user/user.model'
 import { DeleteResult, UpdateResult } from '@shared/dto/typeorm-result.dto'
 import { UpdateProductInput } from '@feature/product/dto/update-product.input'
 import { CreateProductInput } from '@feature/product/dto/create-product.input'
