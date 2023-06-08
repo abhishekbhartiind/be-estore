b863e8e380c479297b6e5bdf4e6d82a3afa29416
erenustun <eren.uestuen@gmail.com>
2023-06-08 03:03:38 +0200

    fix: prettier formatting

--- src/shared/decorator/current-user.decorator.ts
+++ src/shared/decorator/current-user.decorator.ts
@@ -2,12 +2,10 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 import { getUserFromHeader } from '@shared/util/api.util'
 
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext) => {
-    if (context.getType() === 'http') {
-      return context.switchToHttp().getRequest().user
-    }
+export const CurrentUser = createParamDecorator((_data: unknown, context: ExecutionContext) => {
+  if (context.getType() === 'http') {
+    return context.switchToHttp().getRequest().user
+  }
 
-    return getUserFromHeader(context)
-  },
-)
+  return getUserFromHeader(context)
+})
