e6168d2417e4d53e18e08397e3b1d6fb4c965d40
erenustun <eren.uestuen@gmail.com>
2023-06-08 15:06:53 +0200

    feat: util (helper) to pipeline returned products data (puts ratings into array)

---
+++ src/features/product/util/rating-average.util.ts
@@ -0,0 +1,46 @@
+import { PaginationArgs } from '@feature/product/dto/pagination.args'
+import {
+  Product,
+  ProductsFetchResponse,
+} from '@feature/product/model/product.model'
+import { ProductRating } from '@feature/product/model/rating.model'
+import { FilterArgs } from '@feature/product/dto/filter.args'
+
+export const withRating = (product: Product): Product => {
+  let ratingTotal = 0
+  const ratingCount = Array.isArray(product.rating) && product.rating.length
+
+  product?.rating?.map((rating: ProductRating) => {
+    ratingTotal = rating.star + ratingTotal
+  })
+  product.ratingAverage = ratingTotal / Number(ratingCount)
+
+  return product
+}
+
+export const withRatingArray = (
+  products: Product[],
+  paginationArgs: PaginationArgs,
+  count: number,
+  filter?: FilterArgs,
+): ProductsFetchResponse => {
+  const { page, limit } = paginationArgs
+  products.map((product: Product, i: number) => {
+    if (product.rating && product.rating?.length > 0) {
+      const ratingCount =
+        Array.isArray(products[i].rating) && products[i]?.rating?.length
+      let ratingTotal = 0
+      products[i]?.rating?.map((rating: ProductRating) => {
+        ratingTotal = rating.star + ratingTotal
+      })
+      products[i].ratingAverage = ratingTotal / Number(ratingCount)
+    }
+  })
+  return {
+    data: products,
+    page,
+    limit,
+    count,
+    ...(filter && filter),
+  }
+}
