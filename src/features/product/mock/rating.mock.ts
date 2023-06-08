f4bb649a3ef4f656ff02b1eae44fc05bb518e986
erenustun <eren.uestuen@gmail.com>
2023-06-08 15:05:33 +0200

    feat: mock data for product feature

---
+++ src/features/product/mock/rating.mock.ts
@@ -0,0 +1,16 @@
+export const ratingMock = [
+  {
+    id: '8b6396a4-bcc2-4795-a692-583a857a45f8',
+    star: 4,
+    product: { id: '66279d45-6a09-4c2c-8c7e-12ae8b6844e4' },
+    user: { id: '05cff960-9ebe-4689-8b55-4011a99a6792' },
+    text: "Got a renewed OnePlus 10 Pro phone for $630 before fees. The seller is not allowed to give you the original 80W VOOC charging adapters so that is one drawback - however it isn't the sellers fault. Otherwise, the screen and everything was pristine. There were some scratches on the top of the phone on the metal but my case hides it anyways. There is also something small lodged in one hole of the top speaker grille but it is barely noticeable.",
+  },
+  {
+    id: '73eebe3e-52e8-4462-8d85-abc25f9a857f',
+    star: 5,
+    product: { id: '66279d45-6a09-4c2c-8c7e-12ae8b6844e4' },
+    user: { id: 'f6eef4f4-d904-4b61-8012-2c7c2792c1bb' },
+    text: "Phone looks in phenomenal condition and doesn't have any issues at all. I had to get mines replaced due to battery health being less than 80%. Seller helped out and swapped mines out for a same condition one. Seller was understanding and phone is holding out great! No issues reported at this time. Will buy again if need be",
+  },
+]
