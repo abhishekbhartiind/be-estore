5e921dfe6c731acab29d9f1d2e77c640105ae42a
erenustun <eren.uestuen@gmail.com>
2023-06-15 18:14:30 +0200

    feat: mock data for specification related data

---
+++ src/features/product/mock/specification/cpu.mock.ts
@@ -0,0 +1,8 @@
+export const cpuMock: any[] = [
+  {
+    id: '18d73dbd-f16d-4b3f-8fc5-aafb152d342f',
+    cores: 8,
+    frequency: [3.36, 2.8, 2],
+    name: 'Qualcomm Snapdragon 8 Gen 2',
+  },
+]
