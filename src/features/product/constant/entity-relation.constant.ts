1ecd9bd95e1c66d3d26cb7fef4c532760887a15f
erenustun <eren.uestuen@gmail.com>
2023-06-15 18:15:45 +0200

    feat: relationship for querying product data

--- src/features/product/constant/entity-relation.constant.ts
+++ src/features/product/constant/entity-relation.constant.ts
@@ -4,5 +4,9 @@ export const PRODUCT_RELATIONS = [
   'image',
   'rating',
   'specification',
+  'specification.battery',
+  'specification.connectivity',
+  'specification.cpu',
+  'specification.display',
 ]
 export const RATING_RELATIONS = ['product', 'user']
