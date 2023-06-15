faf63a43422d58506a014f2d2d4ac14c3bd773bd
erenustun <eren.uestuen@gmail.com>
2023-06-16 00:12:57 +0200

    refactor: moved product entity

--- src/features/product/features/brand/brand.model.ts
+++ src/features/product/features/brand/brand.model.ts
@@ -1,10 +1,10 @@
 import { InputType, ObjectType } from '@nestjs/graphql'
 import { Column, Entity, OneToMany } from 'typeorm'
-import { Product } from '@feature/product/product.model'
+import { Product } from '@feature/product/model/product.model'
 import { BaseEntity } from '@shared/models/base.model'
 
 @ObjectType()
-@InputType('ProductBrandInput')
+@InputType('BrandInput')
 @Entity()
 export class Brand extends BaseEntity {
   @Column()
--- src/features/product/features/category/category.model.ts
+++ src/features/product/features/category/category.model.ts
@@ -1,6 +1,6 @@
 import { InputType, ObjectType } from '@nestjs/graphql'
 import { Column, Entity, OneToMany } from 'typeorm'
-import { Product } from '@feature/product/product.model'
+import { Product } from '@feature/product/model/product.model'
 import { BaseEntity } from '@shared/models/base.model'
 
 @ObjectType()
