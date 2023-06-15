f1612fbc8eba8dc02aa3a3aefeac0d81d75a2579
erenustun <eren.uestuen@gmail.com>
2023-06-16 00:24:35 +0200

    feat: fetch methods for brand and category entity

--- src/features/product/features/category/category.module.ts
+++ src/features/product/features/category/category.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Category } from '@feature/product/features/category/category.model'
 import { CategoryService } from '@feature/product/features/category/category.service'
+import { CategoryResolver } from '@feature/product/features/category/category.resolver'
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category])],
-  providers: [CategoryService],
+  providers: [CategoryService, CategoryResolver],
   exports: [CategoryService],
 })
 export class CategoryModule {}
