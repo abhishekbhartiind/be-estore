9569d759377382d9b3e0d338a795171050d7b061
erenustun <eren.uestuen@gmail.com>
2023-06-16 02:27:33 +0200

    refactor: different mock data for certain order states (cancelled, shipped out, etc)

--- src/features/order/mock/order-has-product.mock.ts
+++ src/features/order/mock/order-has-product.mock.ts
@@ -1,8 +1,20 @@
 export const orderHasProductsMock = [
   {
     id: '10602522-f75d-4225-8306-2eb613e48e60',
-    quantity: 1,
+    quantity: 2,
     order: { id: '1e0c5cb7-7854-4244-bbd4-32390d7d0afd' },
+    product: { id: 'ea7882b2-f105-44c8-b0e5-e06a5725125c' },
+  },
+  {
+    id: 'f6b6dd38-7a5b-40dd-b110-897200bddd80',
+    quantity: 3,
+    order: { id: '3fc60450-357c-4752-a763-a32a4f78f1b6' },
+    product: { id: 'ea7882b2-f105-44c8-b0e5-e06a5725125c' },
+  },
+  {
+    id: 'c05de6b0-7c0a-4279-a5a9-54186e2b040c',
+    quantity: 1,
+    order: { id: '9beade45-c8de-43b1-ba2d-48a1570ef7e8' },
     product: { id: 'ea7882b2-f105-44c8-b0e5-e06a5725125c' },
   },
 ]
--- src/features/order/mock/order.mock.ts
+++ src/features/order/mock/order.mock.ts
@@ -1,9 +1,27 @@
 export const orderMock = [
   {
     id: '1e0c5cb7-7854-4244-bbd4-32390d7d0afd',
+    pending: false,
+    total: 2398,
+    user: { id: 'f6eef4f4-d904-4b61-8012-2c7c2792c1bb' },
+    products: [],
+    created: '2023-06-10 17:00:00-02',
+  },
+  {
+    id: '3fc60450-357c-4752-a763-a32a4f78f1b6',
+    pending: false,
+    total: 3597,
+    user: { id: 'f6eef4f4-d904-4b61-8012-2c7c2792c1bb' },
+    products: [],
+    created: '2023-06-15 17:00:00-02',
+    cancelled: '2023-06-15 23:00:00-02',
+  },
+  {
+    id: '9beade45-c8de-43b1-ba2d-48a1570ef7e8',
     pending: true,
     total: 1199,
     user: { id: 'f6eef4f4-d904-4b61-8012-2c7c2792c1bb' },
     products: [],
+    created: new Date(),
   },
 ]
